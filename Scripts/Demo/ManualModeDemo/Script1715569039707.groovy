2013c6418d01986353e5f6526b4bea660de544d6
papah <papah@PapahGilak>
2024-05-15 13:58:14 +0700

    Fourth Commit

--- Scripts/Demo/ManualModeDemo/Script1715569039707.groovy
+++ Scripts/Demo/ManualModeDemo/Script1715569039707.groovy
@@ -17,9 +17,9 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('https://opensource-demo.orangehrmlive.com/')
+WebUI.openBrowser('')
 
-//WebUI.navigateToUrl('https://opensource-demo.orangehrmlive.com/')
+WebUI.navigateToUrl('https://opensource-demo.orangehrmlive.com/')
 
 WebUI.setText(findTestObject('Page_OrangeHRM/input_Username_username'), 'Admin')
 
